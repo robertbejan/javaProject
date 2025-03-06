024854cb54d429bf67b66b6b6423765c185ecd82
bejanrobert.01 <@gmail.com>
2025-03-06 14:00:37 +0200

    third commit

--- src/javaProject/javaClass.java
+++ src/javaProject/javaClass.java
@@ -5,7 +5,7 @@ public class javaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello PIP!");
-		System.out.println("Second commit");
+		System.out.println("Third commit");
 	}
 
 }
