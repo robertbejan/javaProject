f1aec80192f745d5e50cb31d61249ddff1ad8e8d
Bejan Robert <@student.tuiasi.ro>
2025-03-06 13:51:03 +0200

    Second commit

--- src/javaProject/javaClass.java
+++ src/javaProject/javaClass.java
@@ -5,6 +5,7 @@ public class javaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello PIP!");
+		System.out.println("Second commit");
 	}
 
 }
